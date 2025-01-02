98b912cbc33e551758892c72a36aa4743ca4c9e8
{Ajit <ajitashwathr23@lpu.in>
2025-01-02 13:30:38 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import {use, useState} from 'react';
+import {useState} from 'react';
 import Search from '@/components/Search';
 import MovieCard from '@/components/MovieCard';
 import {Movie} from '@/types/movie';
